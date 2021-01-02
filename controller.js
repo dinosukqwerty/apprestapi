8978be09196ddf015ff61bc42a3c11656fdeda19
dinosukqwerty <kunci41@gmail.com>
2021-01-02 14:24:22 +0700

    membuat routes

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var response = require('./res');
-var connnection = require('./koneksi')
+var connnection = require('./koneksi');
 
-exports.index = function[req, res]{
+exports.index = function(req, res){
     response.ok("Aplikasi RestAPI berjalan")
 };
