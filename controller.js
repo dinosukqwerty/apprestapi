c0e95a38b02829c656bbbfd9ca45fa878c0f9e20
dinosukqwerty <kunci41@gmail.com>
2021-01-02 15:21:45 +0700

    berhasil menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -17,3 +17,15 @@ exports.tampilsemua = function(req, res){
         }
     });
 };
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilid = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa=?', [id], function(error, row, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(row, res)
+        }
+    }) 
+}
--- routes.js
+++ routes.js
@@ -10,4 +10,7 @@ module.exports = function(app){
 
     app.route('/tampil')
     .get(jsonku.tampilsemua);
+
+    app.route('/tampil/:id')
+    .get(jsonku.tampilid);
 }
